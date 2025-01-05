3856e74773c57a30a890f7809a5dc192b2a69e63
Mike Thiem <mike.j.thiem@gmail.com>
2025-01-05 13:45:33 -0800

    Implement black/white lists for embed-vault

--- embed-vault.py
+++ embed-vault.py
@@ -1,20 +1,32 @@
-from typing import List
+from typing import Set
 from langchain_community.document_loaders import ObsidianLoader
 from langchain_core.documents import Document
 from langchain_ollama import OllamaEmbeddings
 from langchain.vectorstores import Chroma
+from langchain_community.vectorstores.utils import filter_complex_metadata
+from pathlib import Path
 from os import getenv, path
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from get_vector_db import get_vector_db
 
 class CustomObsidianLoader(ObsidianLoader):
-  # Override the lazy_load method to allow us to specify either a blacklist or whitelist of files to load
-  def lazy_load(self, blacklist: List[str] | None = None, whitelist: List[str] | None = None):
-    paths = list(path(self.file_path).glob("**/*.md"))
+  def __init__(self, path: str | Path, encoding: str = "UTF-8", collect_metadata: bool = True, blacklist: Set[str] | None = None, whitelist: Set[str] | None = None):
+    super().__init__(path, encoding, collect_metadata)
+    self.blacklist = blacklist
+    self.whitelist = whitelist
+
+  def lazy_load(self):
+    paths = list(Path(self.file_path).glob("**/*.md"))
     for path in paths:
-      if blacklist and path in blacklist:
+      if self.blacklist and path in self.blacklist:
+        print(f"Skipping blacklisted {path}")
         continue
-      if whitelist and path not in whitelist:
+      if self.whitelist and path not in self.whitelist:
+        print(f"Skipping non-whitelisted {path}")
         continue
+      print(f"Loading {path}")
       with open(path, encoding=self.encoding) as f:
         text = f.read()
       
@@ -33,18 +45,36 @@ class CustomObsidianLoader(ObsidianLoader):
 
 
 # Load Obsidian documents
+load_dotenv()
 vault_path = getenv("OBSIDIAN_VAULT_PATH")
 
 if not vault_path or not path.isdir(vault_path):
   raise ValueError(f"Invalid or missing OBSIDIAN_VAULT_PATH: '{vault_path}'")
 
 # Loads only markdown files, not media or PDFs
-blacklist = ["_notion-like-tables/", "Excalidraw/", 'MEDIA/', 'lib/', 'TEMPLATES/', 'File Transfer/']
-loader = CustomObsidianLoader(vault_path, whitelist=[])
+# blacklist = set(["_notion-like-tables/", "Excalidraw/", 'MEDIA/', 'lib/', 'TEMPLATES/', 'File Transfer/'])
+# whitelist = set(["Work/Foodee/"])
+loader = CustomObsidianLoader(vault_path)#, blacklist=blacklist, whitelist=whitelist)
+print('Loading documents...')
 docs = loader.load()
 
-# Create embeddings using Ollama
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+# Convert complex metadata types to strings
+filtered_docs = filter_complex_metadata(docs)
+# # Convert remaining dict values to strings
+# for filtered_doc in filtered_docs:
+#     filtered_doc.metadata = {k: str(v) for k, v in filtered_doc.metadata.items()}
 
-# Create vector store
-vectorstore = Chroma.from_documents(docs, embeddings)
+
+# print('Chunking documents...')
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
+# chunks = []
+# for doc in docs:
+#   chunks.extend(text_splitter.split_text(doc.page_content))
+
+# Store embeddings in Chroma
+print('Computing and storing embeddings...')
+db = get_vector_db()
+db.add_documents(filtered_docs)
+db.persist()
+
+print('Done!')
